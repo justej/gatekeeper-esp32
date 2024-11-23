f53bdfbc27bd85609622377d5ed6d4a699b26f5f
Oleksandr Iezhov <oiezhov@gmail.com>
2024-11-23 14:10:42 +0200

    Extracted Wi-Fi connection logic to separate files

---
+++ main/include/wifi_connect.h
@@ -0,0 +1,11 @@
+#ifndef _WIFI_CONNECT_H_
+
+#include "esp_event.h"
+#include "esp_log.h"
+#include "esp_netif.h"
+#include "esp_wifi.h"
+
+esp_err_t connect_to_wifi(wifi_config_t *wifi_config);
+void disconnect_from_wifi(void);
+
+#endif // _WIFI_CONNECT_H_
--- main/main.c
+++ main/main.c
@@ -3,11 +3,8 @@
 
 #include "esp_sntp.h"
 #include "esp_netif_sntp.h"
-#include "esp_netif.h"
-#include "esp_wifi.h"
 #include "esp_tls.h"
 #include "esp_crt_bundle.h"
-#include "esp_event.h"
 #include "esp_timer.h"
 #include "esp_log.h"
 
@@ -15,23 +12,16 @@
 #include "nvs.h"
 
 #include "sdkconfig.h"
+
 #include "secrets.h"
+#include "wifi_connect.h"
 
-
-#define NETIF_DESC_STA "gatekeeper_netif_sta"
-#define WIFI_CONN_MAX_RETRY 5
 
 #define WEB_SERVER "api.telegram.org"
 #define WEB_PORT "443"
 #define WEB_URL "https://" WEB_SERVER "/bot" BOT_TOKEN "/getUpdates?offset=0&limit=10"
 
 
-static esp_netif_t *s_example_sta_netif = NULL;
-static SemaphoreHandle_t s_semph_get_ip_addrs = NULL;
-#if CONNECT_IPV6
-static SemaphoreHandle_t s_semph_get_ip6_addrs = NULL;
-#endif
-static int s_retry_num = 0;
 static const char TAG[] = "gatekeeper";
 
 #define TIME_PERIOD (86400000000ULL)
@@ -42,213 +32,6 @@ static const char TELEGRAM_REQUEST[] = "GET " WEB_URL " HTTP/1.1\r\n"
                              "Connection: close\r\n"
                              "\r\n";
 
-static void on_wifi_connect(void *esp_netif, esp_event_base_t event_base, int32_t event_id, void *event_data);
-static void on_wifi_disconnect(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data);
-static void wifi_start(void);
-esp_err_t wifi_sta_do_connect(wifi_config_t wifi_config, bool wait);
-esp_err_t wifi_sta_do_disconnect(void);
-void disconnect_from_wifi(void);
-void wifi_stop(void);
-static void on_sta_got_ip(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data);
-#if CONNECT_IPV6
-static void example_handler_on_sta_got_ipv6(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data);
-#endif
-bool is_our_netif(const char *prefix, esp_netif_t *netif);
-void print_all_netif_ips(const char *prefix);
-static esp_err_t connect_to_wifi(void);
-
-static void on_wifi_connect(void *esp_netif, esp_event_base_t event_base, int32_t event_id, void *event_data) {
-#if CONNECT_IPV6
-    esp_netif_create_ip6_linklocal(esp_netif);
-#endif // CONNECT_IPV6
-}
-
-bool is_our_netif(const char *prefix, esp_netif_t *netif) {
-    return strncmp(prefix, esp_netif_get_desc(netif), strlen(prefix) - 1) == 0;
-}
-
-static esp_err_t print_all_ips_tcpip(void* ctx) {
-    const char *prefix = ctx;
-    // iterate over active interfaces, and print out IPs of "our" netifs
-    esp_netif_t *netif = NULL;
-    while ((netif = esp_netif_next_unsafe(netif)) != NULL) {
-        if (is_our_netif(prefix, netif)) {
-            ESP_LOGI(TAG, "Connected to %s", esp_netif_get_desc(netif));
-#if CONFIG_LWIP_IPV4
-            esp_netif_ip_info_t ip;
-            ESP_ERROR_CHECK(esp_netif_get_ip_info(netif, &ip));
-
-            ESP_LOGI(TAG, "- IPv4 address: " IPSTR ",", IP2STR(&ip.ip));
-#endif
-#if CONNECT_IPV6
-            esp_ip6_addr_t ip6[MAX_IP6_ADDRS_PER_NETIF];
-            int ip6_addrs = esp_netif_get_all_ip6(netif, ip6);
-            for (int j = 0; j < ip6_addrs; ++j) {
-                esp_ip6_addr_type_t ipv6_type = esp_netif_ip6_get_addr_type(&(ip6[j]));
-                ESP_LOGI(TAG, "- IPv6 address: " IPV6STR ", type: %s", IPV62STR(ip6[j]), example_ipv6_addr_types_to_str[ipv6_type]);
-            }
-#endif
-        }
-    }
-    return ESP_OK;
-}
-
-void print_all_netif_ips(const char *prefix) {
-    // Print all IPs in TCPIP context to avoid potential races of removing/adding netifs when iterating over the list
-    esp_netif_tcpip_exec(print_all_ips_tcpip, (void*) prefix);
-}
-
-static void on_sta_got_ip(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data) {
-    s_retry_num = 0;
-    ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
-    if (!is_our_netif(NETIF_DESC_STA, event->esp_netif)) {
-        return;
-    }
-    ESP_LOGI(TAG, "Got IPv4 event: Interface \"%s\" address: " IPSTR, esp_netif_get_desc(event->esp_netif), IP2STR(&event->ip_info.ip));
-    if (s_semph_get_ip_addrs) {
-        xSemaphoreGive(s_semph_get_ip_addrs);
-    } else {
-        ESP_LOGI(TAG, "- IPv4 address: " IPSTR ",", IP2STR(&event->ip_info.ip));
-    }
-}
-
-#if CONNECT_IPV6
-static void example_handler_on_sta_got_ipv6(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data) {
-    ip_event_got_ip6_t *event = (ip_event_got_ip6_t *)event_data;
-    if (!is_our_netif(NETIF_DESC_STA, event->esp_netif)) {
-        return;
-    }
-    esp_ip6_addr_type_t ipv6_type = esp_netif_ip6_get_addr_type(&event->ip6_info.ip);
-    ESP_LOGI(TAG, "Got IPv6 event: Interface \"%s\" address: " IPV6STR ", type: %s", esp_netif_get_desc(event->esp_netif),
-             IPV62STR(event->ip6_info.ip), example_ipv6_addr_types_to_str[ipv6_type]);
-
-    if (ipv6_type == EXAMPLE_CONNECT_PREFERRED_IPV6_TYPE) {
-        if (s_semph_get_ip6_addrs) {
-            xSemaphoreGive(s_semph_get_ip6_addrs);
-        } else {
-            ESP_LOGI(TAG, "- IPv6 address: " IPV6STR ", type: %s", IPV62STR(event->ip6_info.ip), example_ipv6_addr_types_to_str[ipv6_type]);
-        }
-    }
-}
-#endif // CONNECT_IPV6
-
-static void wifi_start(void) {
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-
-    esp_netif_inherent_config_t esp_netif_config = ESP_NETIF_INHERENT_DEFAULT_WIFI_STA();
-    // Warning: the interface desc is used in tests to capture actual connection details (IP, gw, mask)
-    esp_netif_config.if_desc = NETIF_DESC_STA;
-    esp_netif_config.route_prio = 128;
-    s_example_sta_netif = esp_netif_create_wifi(WIFI_IF_STA, &esp_netif_config);
-    esp_wifi_set_default_wifi_sta_handlers();
-
-    ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
-    ESP_ERROR_CHECK(esp_wifi_start());
-}
-
-esp_err_t wifi_sta_do_disconnect(void) {
-    ESP_ERROR_CHECK(esp_event_handler_unregister(WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, &on_wifi_disconnect));
-    ESP_ERROR_CHECK(esp_event_handler_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, &on_sta_got_ip));
-    ESP_ERROR_CHECK(esp_event_handler_unregister(WIFI_EVENT, WIFI_EVENT_STA_CONNECTED, &on_wifi_connect));
-#if CONNECT_IPV6
-    ESP_ERROR_CHECK(esp_event_handler_unregister(IP_EVENT, IP_EVENT_GOT_IP6, &example_handler_on_sta_got_ipv6));
-#endif
-    if (s_semph_get_ip_addrs) {
-        vSemaphoreDelete(s_semph_get_ip_addrs);
-    }
-#if CONNECT_IPV6
-    if (s_semph_get_ip6_addrs) {
-        vSemaphoreDelete(s_semph_get_ip6_addrs);
-    }
-#endif
-    return esp_wifi_disconnect();
-}
-
-static void on_wifi_disconnect(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data) {
-    s_retry_num++;
-    if (s_retry_num > WIFI_CONN_MAX_RETRY) {
-        ESP_LOGI(TAG, "WiFi Connect failed %d times, stop reconnect.", s_retry_num);
-        /* let wifi_sta_do_connect() return */
-        if (s_semph_get_ip_addrs) {
-            xSemaphoreGive(s_semph_get_ip_addrs);
-        }
-#if CONNECT_IPV6
-        if (s_semph_get_ip6_addrs) {
-            xSemaphoreGive(s_semph_get_ip6_addrs);
-        }
-#endif
-        wifi_sta_do_disconnect();
-        return;
-    }
-    ESP_LOGI(TAG, "Wi-Fi disconnected, trying to reconnect...");
-    esp_err_t err = esp_wifi_connect();
-    if (err == ESP_ERR_WIFI_NOT_STARTED) {
-        return;
-    }
-    ESP_ERROR_CHECK(err);
-}
-
-esp_err_t wifi_sta_do_connect(wifi_config_t wifi_config, bool wait) {
-    if (wait) {
-        s_semph_get_ip_addrs = xSemaphoreCreateBinary();
-        if (s_semph_get_ip_addrs == NULL) {
-            return ESP_ERR_NO_MEM;
-        }
-#if CONNECT_IPV6
-        s_semph_get_ip6_addrs = xSemaphoreCreateBinary();
-        if (s_semph_get_ip6_addrs == NULL) {
-            vSemaphoreDelete(s_semph_get_ip_addrs);
-            return ESP_ERR_NO_MEM;
-        }
-#endif
-    }
-    s_retry_num = 0;
-    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, &on_wifi_disconnect, NULL));
-    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &on_sta_got_ip, NULL));
-    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, WIFI_EVENT_STA_CONNECTED, &on_wifi_connect, s_example_sta_netif));
-#if CONNECT_IPV6
-    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_GOT_IP6, &example_handler_on_sta_got_ipv6, NULL));
-#endif
-
-    ESP_LOGI(TAG, "Connecting to %s...", wifi_config.sta.ssid);
-    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
-    esp_err_t ret = esp_wifi_connect();
-    if (ret != ESP_OK) {
-        ESP_LOGE(TAG, "WiFi connect failed! ret:%x", ret);
-        return ret;
-    }
-    if (wait) {
-        ESP_LOGI(TAG, "Waiting for IP(s)");
-        xSemaphoreTake(s_semph_get_ip_addrs, portMAX_DELAY);
-#if CONNECT_IPV6
-        xSemaphoreTake(s_semph_get_ip6_addrs, portMAX_DELAY);
-#endif
-        if (s_retry_num > WIFI_CONN_MAX_RETRY) {
-            return ESP_FAIL;
-        }
-    }
-    return ESP_OK;
-}
-
-void wifi_stop(void) {
-    esp_err_t err = esp_wifi_stop();
-    if (err == ESP_ERR_WIFI_NOT_INIT) {
-        return;
-    }
-    ESP_ERROR_CHECK(err);
-    ESP_ERROR_CHECK(esp_wifi_deinit());
-    ESP_ERROR_CHECK(esp_wifi_clear_default_wifi_driver_and_handlers(s_example_sta_netif));
-    esp_netif_destroy(s_example_sta_netif);
-    s_example_sta_netif = NULL;
-}
-
-void disconnect_from_wifi(void)
-{
-    wifi_sta_do_disconnect();
-    wifi_stop();
-}
 
 static void https_get_request(esp_tls_cfg_t cfg, const char* WEB_SERVER_URL, const char* REQUEST) {
     char buf[512];
@@ -316,29 +99,6 @@ static void https_get_request(esp_tls_cfg_t cfg, const char* WEB_SERVER_URL, con
 
 cleanup:
     esp_tls_conn_destroy(tls);
-}
-
-static esp_err_t connect_to_wifi(void) {
-    ESP_LOGI(TAG, "Connecting to Wi-Fi...");
-    wifi_start();
-    wifi_config_t wifi_config = {
-        .sta = {
-            .ssid = WIFI_SSID,
-            .password = WIFI_PASSWORD,
-            .scan_method = WIFI_FAST_SCAN,
-            .sort_method = WIFI_CONNECT_AP_BY_SIGNAL,
-            .threshold.rssi = 1,
-            .threshold.authmode = WIFI_AUTH_WPA_PSK,
-        },
-    };
-
-    if (wifi_sta_do_connect(wifi_config, true) != ESP_OK) {
-        return ESP_FAIL;
-    }
-    ESP_ERROR_CHECK(esp_register_shutdown_handler(&disconnect_from_wifi));
-    
-    print_all_netif_ips(NETIF_DESC_STA);
-    return ESP_OK;
 }
 
 #define STORAGE_NAMESPACE "storage"
@@ -466,7 +226,18 @@ void app_main(void) {
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
 
-    ESP_ERROR_CHECK(connect_to_wifi());
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = WIFI_SSID,
+            .password = WIFI_PASSWORD,
+            .scan_method = WIFI_FAST_SCAN,
+            .sort_method = WIFI_CONNECT_AP_BY_SIGNAL,
+            .threshold.rssi = 1,
+            .threshold.authmode = WIFI_AUTH_WPA_PSK,
+        },
+    };
+    ESP_ERROR_CHECK(connect_to_wifi(&wifi_config));
+    // ESP_ERROR_CHECK(connect_to_wifi(WIFI_SSID, WIFI_PASSWORD));
 
     if (esp_reset_reason() == ESP_RST_POWERON) {
         ESP_LOGI(TAG, "Updating time from NVS");
